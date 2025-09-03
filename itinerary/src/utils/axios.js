80676a22bd7486272ec362dbf5c51a93f3d06077
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2025-09-03 22:34:31 +0530

    axios render added

--- itinerary/src/utils/axios.js
+++ itinerary/src/utils/axios.js
@@ -4,9 +4,10 @@ import axios from "axios";
 // Create the axios instance
 const api = axios.create({
 
-  baseURL: "http://localhost:8080/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://itinerary-server-472k.onrender.com",
 //   withCredentials: true, // Replace with your API base URL
-  // withCredentials: true, // Only needed if you're using cookies with auth
+  withCredentials: true, // Only needed if you're using cookies with auth
 });
 
 export default api;
