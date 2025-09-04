e0cb962743a13396659574d503a86df0c44e8623
KushalRastogi20 <138278696+KushalRastogi20@users.noreply.github.com>
2025-09-04 11:35:43 +0530

    axios updated

--- itinerary/src/utils/axios.js
+++ itinerary/src/utils/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const api = axios.create({
 
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "https://itinerary-server-472k.onrender.com/api/v1",
+  baseURL:"https://itinerary-server-4x3u.onrender.com/api/v1",
 //   withCredentials: true, // Replace with your API base URL
   withCredentials: true, // Only needed if you're using cookies with auth
 });
