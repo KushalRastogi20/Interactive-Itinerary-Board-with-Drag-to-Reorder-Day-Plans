9e0e52d4dec226a8247d008c8f2ef204e1c97938
Kushal Rastogi <138278696+KushalRastogi20@users.noreply.github.com>
2025-11-07 09:48:42 +0530

    Switch API base URL to production server

--- itinerary/src/utils/axios.js
+++ itinerary/src/utils/axios.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // Create the axios instance
 const api = axios.create({
 
-  baseURL: "http://localhost:8080/api/v1",
-  // baseURL:"https://itinerary-server-zqup.onrender.com/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
+  baseURL:"https://itinerary-server-zqup.onrender.com/api/v1",
 //   withCredentials: true, // Replace with your API base URL
   withCredentials: true, // Only needed if you're using cookies with auth
 });
